1ff89066a52c06b53a895989acbcb1359ad92125
OlenaKravchenko2022 <107769679+OlenaKravchenko2022@users.noreply.github.com>
2022-11-16 16:36:41 +0100

    debug

--- src/phonetic.js
+++ src/phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="phonetic">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+      <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
         Listen
       </a>
       <br />

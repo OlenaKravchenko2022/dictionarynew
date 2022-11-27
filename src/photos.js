07affacd2f6de3ed810a2e19f9d6b90c582e8dda
OlenaKravchenko2022 <107769679+OlenaKravchenko2022@users.noreply.github.com>
2022-11-27 21:33:13 +0100

    debuging

--- src/photos.js
+++ src/photos.js
@@ -1,13 +1,23 @@
 import React from "react";
+import "./photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
-    console.log(props.photos);
     return (
       <section className="Photos">
-        {props.photos.map(function (photo, index) {
-          return <img src={photo.src.landscape} key={index} />;
-        })}
+        <div className="row">
+          {" "}
+          {props.photos.map(function (photo, index) {
+            console.log(photo);
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape} className="img-fluid" />
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </section>
     );
   } else {

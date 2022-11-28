722470001a92507ea68560b912a86c3ca3df06a6
OlenaKravchenko2022 <107769679+OlenaKravchenko2022@users.noreply.github.com>
2022-11-28 22:16:44 +0100

    Update photos.js

--- src/photos.js
+++ src/photos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
@@ -8,7 +9,7 @@ export default function Photos(props) {
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
-                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                <a href={photo.src.original}>
                   <img
                     src={photo.src.landscape}
                     className="img-fluid"

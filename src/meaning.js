69554caba6db4edc99642f5e2015a3bff34dd3bc
OlenaKravchenko2022 <107769679+OlenaKravchenko2022@users.noreply.github.com>
2022-11-28 20:12:16 +0100

    Update meaning.js

--- src/meaning.js
+++ src/meaning.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Synonyms from "./Synonyms.js";
-import "./meaning.css";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);

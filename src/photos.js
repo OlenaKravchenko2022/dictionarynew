5c76b13a969cba1fe86e74cc8dbd58db707fc2ec
OlenaKravchenko2022 <107769679+OlenaKravchenko2022@users.noreply.github.com>
2022-11-28 21:27:49 +0100

    Update photos.js

--- src/photos.js
+++ src/photos.js
@@ -6,13 +6,15 @@ export default function Photos(props) {
     return (
       <section className="Photos">
         <div className="row">
-          {" "}
           {props.photos.map(function (photo, index) {
             return (
               <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
-                  image
-                  <img src={photo.src.landscape} className="img" alt="img" />
+                  <img
+                    src={photo.src.landscape}
+                    className="img-fluid"
+                    alt="img"
+                  />
                 </a>
               </div>
             );

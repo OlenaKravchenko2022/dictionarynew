a3131d31d6d5c57c2fb46b98c152846e0572566d
OlenaKravchenko2022 <107769679+OlenaKravchenko2022@users.noreply.github.com>
2022-11-28 22:07:34 +0100

    delete photo.css

--- src/photos.js
+++ src/photos.js
@@ -1,14 +1,13 @@
 import React from "react";
-import "./Photos.css";
 
 export default function Photos(props) {
   if (props.photos) {
     return (
       <section className="Photos">
-        <div className="row col-4">
+        <div className="row ">
           {props.photos.map(function (photo, index) {
             return (
-              <div className="row col-4" key={index}>
+              <div className="col-4" key={index}>
                 <a href={photo.src.original} target="_blank" rel="noreferrer">
                   <img
                     src={photo.src.landscape}
